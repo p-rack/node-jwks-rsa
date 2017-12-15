b15d06077ba3d3858e3516e1490851be167946b7
Rack <u1305@IT-MAC-13184.local>
2017-12-15 17:21:16 +0100

    updated index.d.ts and added precompiled  lib folder for git installation

--- index.d.ts
+++ index.d.ts
@@ -31,9 +31,13 @@ declare module 'jwks-rsa' {
       handleSigningKeyError?(err: Error, cb: (err: Error) => void): any;
     }
 
-    function ExpressJwtSecret(options: JwksRsa.Options): ExpressJwt.SecretCallback;
+    function expressJwtSecret(options: JwksRsa.Options): ExpressJwt.SecretCallback;
 
-    function HapiJwt2Key(options: JwksRsa.Options): (name: string, scheme: string, options?: any) => void;
+    function passportJwtSecret(options: JwksRsa.Options): ExpressJwt.SecretCallback;
+
+    function hapiJwt2Key(options: JwksRsa.Options): (name: string, scheme: string, options?: any) => void;
+
+    function koaJwtSecret(options: JwksRsa.Options): (name: string, scheme: string, options?: any) => void;
 
     class ArgumentError extends Error {
       constructor(message: string);
